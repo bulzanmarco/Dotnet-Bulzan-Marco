ba2326427540a61139b2217444dad2306157fc39
Bulzan Marco <marco.bulzan@student.upt.ro>
2021-11-02 10:54:22 +0200

    Video 5

---
+++ C#/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program ceva = new Program();
+            ceva.Print();
+        }
+        void Print()
+        {
+            Console.WriteLine("Hello World!");// comentariu
+            Console.WriteLine("Best friend!");
+            Console.WriteLine("Hey!");
+        }
+    }
+}
+// create the method
+//invoke the method
+//namespace->class->members->method->statements
