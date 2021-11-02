e0e49f3712be116646e12bcfd075c8c0e47b0800
Bulzan Marco <marco.bulzan@student.upt.ro>
2021-11-02 11:30:30 +0200

    Video9

--- C#/ConsoleApp2/Program.cs
+++ C#/ConsoleApp2/Program.cs
@@ -6,12 +6,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int k = 2;//declaratie+initializare
+            int x = -5;
+            uint y = 5;
+            char z = 'P';
+            float o= 5.5f;
+            double e = 4.5;
+            decimal f = 5.2m;
+            bool r = true;
+            string k = "Heyyyy";
 
-            int? y=null;
-            Console.WriteLine(y);
 
-            
+
         }  
     }
 }
